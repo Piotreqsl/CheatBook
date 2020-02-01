12ddfa34772574cc7c2549cd60bf417fd00aa42c
Piotr <piotreksl2002@go2.pl>
2020-02-01 21:02:23 +0100

    Handle everything

--- app/src/main/java/com/onionv2/cheatbook/GooglyFaceTracker.java
+++ app/src/main/java/com/onionv2/cheatbook/GooglyFaceTracker.java
@@ -15,9 +15,16 @@
  */
 package com.onionv2.cheatbook;
 
+import android.content.Context;
 import android.graphics.PointF;
+import android.os.Debug;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.onionv2.cheatbook.camera.GraphicOverlay;
+
 import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
@@ -26,57 +33,27 @@ import com.google.android.gms.vision.face.Landmark;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Tracks the eye positions and state over time, managing an underlying graphic which renders googly
- * eyes over the source video.<p>
- *
- * To improve eye tracking performance, it also helps to keep track of the previous landmark
- * proportions relative to the detected face and to interpolate landmark positions for future
- * updates if the landmarks are missing.  This helps to compensate for intermediate frames where the
- * face was detected but one or both of the eyes were not detected.  Missing landmarks can happen
- * during quick movements due to camera image blurring.
- */
+
 class GooglyFaceTracker extends Tracker<Face> {
     private static final float EYE_CLOSED_THRESHOLD = 0.4f;
-
-    private GraphicOverlay mOverlay;
-    private GooglyEyesGraphic mEyesGraphic;
-
-    // Record the previously seen proportions of the landmark locations relative to the bounding box
-    // of the face.  These proportions can be used to approximate where the landmarks are within the
-    // face bounding box if the eye landmark is missing in a future update.
     private Map<Integer, PointF> mPreviousProportions = new HashMap<>();
-
-    // Similarly, keep track of the previous eye open state so that it can be reused for
-    // intermediate frames which lack eye landmarks and corresponding eye state.
     private boolean mPreviousIsLeftOpen = true;
     private boolean mPreviousIsRightOpen = true;
+
+
 
 
     //==============================================================================================
     // Methods
     //==============================================================================================
 
-    GooglyFaceTracker(GraphicOverlay overlay) {
-        mOverlay = overlay;
-    }
+    GooglyFaceTracker(){}
 
-    /**
-     * Resets the underlying googly eyes graphic and associated physics state.
-     */
-    @Override
-    public void onNewItem(int id, Face face) {
-        mEyesGraphic = new GooglyEyesGraphic(mOverlay);
-    }
 
-    /**
-     * Updates the positions and state of eyes to the underlying graphic, according to the most
-     * recent face detection results.  The graphic will render the eyes and simulate the motion of
-     * the iris based upon these changes over time.
-     */
+
     @Override
     public void onUpdate(FaceDetector.Detections<Face> detectionResults, Face face) {
-        mOverlay.add(mEyesGraphic);
+
 
         updatePreviousProportions(face);
 
@@ -101,31 +78,45 @@ class GooglyFaceTracker extends Tracker<Face> {
             mPreviousIsRightOpen = isRightOpen;
         }
 
-        mEyesGraphic.updateEyes(leftPosition, isLeftOpen, rightPosition, isRightOpen);
+
+        if(isLeftOpen && isRightOpen){setStatus("Both open");}
+        if(!isLeftOpen && isRightOpen){setStatus("Left open");}
+        if(isLeftOpen && !isRightOpen){setStatus("Right open");}
+
+
+
     }
 
-    /**
-     * Hide the graphic when the corresponding face was not detected.  This can happen for
-     * intermediate frames temporarily (e.g., if the face was momentarily blocked from
-     * view).
-     */
+
+    @Override
+    public void onNewItem(int faceId, Face face) {
+    Log.d("hujjjjjj", "hujjj");
+    }
+
     @Override
     public void onMissing(FaceDetector.Detections<Face> detectionResults) {
-        mOverlay.remove(mEyesGraphic);
+        setStatus("Nothing detected");
     }
 
-    /**
-     * Called when the face is assumed to be gone for good. Remove the googly eyes graphic from
-     * the overlay.
-     */
-    @Override
-    public void onDone() {
-        mOverlay.remove(mEyesGraphic);
-    }
+
+
 
     //==============================================================================================
     // Private
     //==============================================================================================
+
+
+
+    private void setStatus(final String text){
+        new Handler(Looper.getMainLooper()).post(new Runnable(){
+            @Override
+            public void run() {
+                CameraActivity.status.setText(text);
+            }
+        });
+
+
+    }
 
     private void updatePreviousProportions(Face face) {
         for (Landmark landmark : face.getLandmarks()) {
