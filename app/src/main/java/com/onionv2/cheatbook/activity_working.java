5c6bf41026880f3cd0dbaabfa7670b959e41767f
Okwas <naimadokwas@gmail.com>
2020-02-16 17:01:31 +0100

    ux improvements

---
+++ app/src/main/java/com/onionv2/cheatbook/activity_working.java
@@ -0,0 +1,14 @@
+package com.onionv2.cheatbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_working extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_working);
+    }
+}
